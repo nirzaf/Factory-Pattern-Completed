ca6de6ade21d2505f0751e23c1bca1c4944f007a
nirzaf <mfmfazrin1986@gmail.com>
2021-12-25 12:08:50 +0530

    Add project files.

---
+++ Business/ShoppingCart.cs
@@ -0,0 +1,91 @@
+﻿using Factory_Pattern_First_Look.Business.Models.Commerce;
+using Factory_Pattern_First_Look.Business.Models.Shipping;
+using System;
+
+namespace Factory_Pattern_First_Look.Business
+{
+    public class ShoppingCart
+    {
+        private readonly Order order;
+
+        public ShoppingCart(Order order)
+        {
+            this.order = order;
+        }
+
+        public string Finalize()    
+        {
+            #region Create Shipping Provider
+            ShippingProvider shippingProvider;
+
+            if (order.Sender.Country == "Australia")
+            {
+                #region Australia Post Shipping Provider
+                var shippingCostCalculator = new ShippingCostCalculator(
+                    internationalShippingFee: 250,
+                    extraWeightFee: 500
+                )
+                {
+                    ShippingType = ShippingType.Standard
+                };
+
+                var customsHandlingOptions = new CustomsHandlingOptions
+                {
+                    TaxOptions = TaxOptions.PrePaid
+                };
+
+                var insuranceOptions = new InsuranceOptions
+                {
+                    ProviderHasInsurance = false,
+                    ProviderHasExtendedInsurance = false,
+                    ProviderRequiresReturnOnDamange = false
+                };
+
+                shippingProvider = new AustraliaPostShippingProvider("CLIENT_ID",
+                    "SECRET",
+                    shippingCostCalculator,
+                    customsHandlingOptions,
+                    insuranceOptions);
+                #endregion
+            }
+            else if (order.Sender.Country == "Sweden")
+            {
+                #region Swedish Postal Service Shipping Provider
+                var shippingCostCalculator = new ShippingCostCalculator(
+                    internationalShippingFee: 50,
+                    extraWeightFee: 100
+                )
+                {
+                    ShippingType = ShippingType.Express
+                };
+
+                var customsHandlingOptions = new CustomsHandlingOptions
+                {
+                    TaxOptions = TaxOptions.PayOnArrival
+                };
+
+                var insuranceOptions = new InsuranceOptions
+                {
+                    ProviderHasInsurance = true,
+                    ProviderHasExtendedInsurance = false,
+                    ProviderRequiresReturnOnDamange = false
+                };
+
+                shippingProvider = new SwedishPostalServiceShippingProvider("API_KEY",
+                    shippingCostCalculator,
+                    customsHandlingOptions,
+                    insuranceOptions);
+                #endregion
+            }
+            else
+            {
+                throw new NotSupportedException("No shipping provider found for origin country");
+            }
+            #endregion
+
+            order.ShippingStatus = ShippingStatus.ReadyForShippment;
+
+            return shippingProvider.GenerateShippingLabelFor(order);
+        }
+    }
+}
